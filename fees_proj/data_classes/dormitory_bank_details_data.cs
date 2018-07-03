d17177f94a5a3a5d14adbfe99cfb7458283f359d
Musa Jahun <msjahun@live.com>
2018-07-04 00:41:43 +0300

    Add project files.

---
+++ fees_proj/data_classes/dormitory_bank_details_data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fees_proj.data_classes
+{
+    public class dormitory_bank_details_data
+    {
+      
+
+
+
+        public string dormitory_name { get; set; } 
+            public string bank_name { get; set; }
+        public string bank_currency { get; set; }
+        public string account_parameter_en { get; set; }
+        public string account_parameter_TR { get; set; }
+        public string account_parameter_value_en { get; set; }
+        public string account_parameter_value_tr { get; set; }
+
+    }
+}
---
+++ fees_proj/data_classes/facility_data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fees_proj.data_classes
+{
+    class facility_data
+    {
+
+        public facility_data(
+        String facility_icon_url,
+        String facility_title_en,
+        String facility_title_tr,
+        ArrayList facility_options_list
+       )
+        {
+            this.facility_icon_url = facility_icon_url;
+            this.facility_title_en = facility_title_en;
+            this.facility_title_tr = facility_title_tr;
+            this.facility_options_list = facility_options_list;
+
+
+        }
+
+        public String facility_icon_url { get; set; }
+        public String facility_title_en { get; set; }
+        public String facility_title_tr { get; set; }
+        public ArrayList facility_options_list { get; set; }
+
+
+    }
+}
